10ba9264ffc5304255ad1d234bad7a0dbf456933
Jay-R Magdaluyo <j.magdaluyo@seiko-it.com.ph.local>
2018-02-23 17:49:15 +0800

    IM

---
+++ resources/views/unauth.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-danger">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    Unauthorized User!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
